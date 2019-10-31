6ff905f74210c9f411c315cb818f0a569ace0ec4
p1mps <imparato.andrea@gmail.com>
2019-10-31 09:40:03 +0100

    removed figwheel

--- test/clj_40k_tournaments/core_test.cljs
+++
@@ -1,3 +0,0 @@
-(ns clj-40k-tournaments.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]))
--- test/clj_40k_tournaments/test_runner.cljs
+++
@@ -1,9 +0,0 @@
-;; This test runner is intended to be run from the command line
-(ns clj-40k-tournaments.test-runner
-  (:require
-    ;; require all the namespaces that you want to test
-    [clj-40k-tournaments.core-test]
-    [figwheel.main.testing :refer [run-tests-async]]))
-
-(defn -main [& args]
-  (run-tests-async 5000))
