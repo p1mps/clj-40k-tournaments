a30dac34d12a4ccc09e2e616c041a7dcc258623e
Andrea Imparato <andrea.imparato@metronom.com>
2020-01-12 20:05:02 +0100

    update db with test user

--- src/middleware.clj
+++ src/middleware.clj
@@ -4,5 +4,5 @@
 (defn auth [handler]
   (fn [request]
     (if (some? (get-in request [:session :member/email]))
-               (handler request)
-               (coast/unauthorized "HAL9000 says: I'm sorry Dave, I can't let you do that"))))
+      (handler request)
+      (coast/unauthorized "Unauthrozied request"))))
